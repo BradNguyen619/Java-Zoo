ec9e17a2e4cb14ffe6a08062f1ac3fdcb543c77f
Brad Nguyen <brad.nguyen@mrjaffesclass.com>
2024-01-04 11:21:37 -0800

    Working

---
+++ NguyenBrad.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class NguyenBrad here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NguyenBrad extends Animal
+{
+  public NguyenBrad() {
+    super("Nguyen Brad", "orange", 4);
+  }
+  
+  public String getType() {
+    return "Cat";
+  }
+  
+  public String getName() {
+      return "Chairman Meow";
+  }
+  
+  public String speak() {
+      return "meow";
+  }
+  
+  public String getDiposition() {
+      return "cat";
+  }
+}
